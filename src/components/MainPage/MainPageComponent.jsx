2fe0aa6c38107d54458bc894340efdbbd6d80579
Caetan <caetan.tojeiro.carpente@cern.ch>
2023-05-12 19:36:03 +0200

    Scaffolding and installing deps

---
+++ src/components/MainPage/MainPageComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react';
+
+
+const MainPageComponent = () => {
+  return (
+    <Header>Hello World</Header>
+  );
+};
+
+export default MainPageComponent;
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainPageComponent from '../../components/MainPage/MainPageComponent';
+import {Container} from 'semantic-ui-react';
+
+const MainPage = () => {
+  return (
+    <Container>
+      <MainPageComponent />
+    </Container>
+  );
+};
+
+export default MainPage;
